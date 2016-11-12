8ae26f7094a43cb1b16ba04cb8357f466535d246
Harry Yoon <harryyoon@outlook.com>
2016-11-11 17:54:08 -0800

    Hough line transform tutorial....

--- CVTutor.ImgProc/CVTutor.ImgProc.CannyDetector01/CVTutor.ImgProc.CannyDetector01.cpp
+++ CVTutor.ImgProc/CVTutor.ImgProc.CannyDetector01/CVTutor.ImgProc.CannyDetector01.cpp
@@ -2,7 +2,7 @@
 //
 
 #include "stdafx.h"
-#include <opencv2/imgproc.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/imgcodecs.hpp>
 
 using namespace std;
--- CVTutor.ImgProc/CVTutor.ImgProc.HoughLine01/CVTutor.ImgProc.HoughLine01.cpp
+++ CVTutor.ImgProc/CVTutor.ImgProc.HoughLine01/CVTutor.ImgProc.HoughLine01.cpp
@@ -2,10 +2,78 @@
 //
 
 #include "stdafx.h"
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+
+using namespace std;
+using namespace cv;
 
 
-int main()
+int main(int argc, char** argv)
 {
-    return 0;
+	string imageName("../../sample-data/pic5.png");
+	if (argc > 1)
+	{
+		imageName = argv[1];
+	}
+
+	Mat src = imread(imageName);
+	if (!src.data)
+	{
+		cerr << "Failed to load image: " << imageName << endl;
+		system("pause");
+		return -1;
+	}
+
+	Mat dst, cdst;
+	Canny(src, dst, 50, 200, 3);
+	cvtColor(dst, cdst, COLOR_GRAY2BGR);
+
+	int threshold = 100;
+	if (argc > 2) {
+		threshold = stoi(argv[2]);
+	}
+
+	auto useP = true;
+	// auto useP = false;
+	if (argc > 3) {
+		if (argv[3] == "p" || argv[3] == "P") {
+			useP = true;
+		}
+	}
+
+	if (useP)
+	{
+		vector<Vec4i> lines;
+		HoughLinesP(dst, lines, 1, CV_PI / 180, threshold, 50, 10);
+		for (size_t i = 0; i < lines.size(); i++)
+		{
+			Vec4i l = lines[i];
+			line(cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0, 0, 255), 3, LINE_AA);
+		}
+	}
+	else {
+		vector<Vec2f> lines;
+		HoughLines(dst, lines, 1, CV_PI / 180, threshold, 0, 0);
+
+		for (size_t i = 0; i < lines.size(); i++)
+		{
+			float rho = lines[i][0], theta = lines[i][1];
+			Point pt1, pt2;
+			double a = cos(theta), b = sin(theta);
+			double x0 = a*rho, y0 = b*rho;
+			pt1.x = cvRound(x0 + 1000 * (-b));
+			pt1.y = cvRound(y0 + 1000 * (a));
+			pt2.x = cvRound(x0 - 1000 * (-b));
+			pt2.y = cvRound(y0 - 1000 * (a));
+			line(cdst, pt1, pt2, Scalar(0, 0, 255), 3, CV_AA);
+		}
+	}
+
+	imshow("source", src);
+	imshow("detected lines", cdst);
+
+	waitKey();
+	return 0;
 }
 
